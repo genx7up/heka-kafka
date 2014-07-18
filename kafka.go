4cd1287b3ea19d0a04c807e9bed5a0cfb92cc805
Liu Cong <innerr@gmail.com>
2014-07-18 14:37:22 +0800

    Fixed build

--- kafka.go
+++ kafka.go
@@ -1,11 +1,8 @@
 package kafka
 
 import (
-	"bytes"
 	"errors"
-	"net/http"
-	"strconv"
-	"io/ioutil"
+	"strings"
 	"github.com/Shopify/sarama"
 	"github.com/mozilla-services/heka/message"
 	. "github.com/mozilla-services/heka/pipeline"
@@ -34,20 +31,16 @@ func (ao *KafkaOutput) Init(config interface{}) (err error) {
 	if len(ao.addrs) == 1 && len(ao.addrs) == 0 {
 		err = errors.New("invalid address")
 	}
-	ao.init()
+	err = ao.init()
 	return
 }
 
 func (ao *KafkaOutput) Run(or OutputRunner, h PluginHelper) (err error) {
 	inChan := or.InChan()
 	errChan := ao.producer.Errors()
-	url := "http://" + ao.config.Address + "/put?topic=" + ao.config.Topic
 
 	var pack *PipelinePack
 	var msg *message.Message
-	var body []byte
-	var respData []byte
-	var resp *http.Response
 
 	ok := true
 	for ok {
@@ -57,15 +50,15 @@ func (ao *KafkaOutput) Run(or OutputRunner, h PluginHelper) (err error) {
 				break
 			}
 			msg = pack.Message
-			err = ao.producer.QueueMessage(ao.config.Topic, sarama.ByteEncoder(msg.GetPayload()))
+			err = ao.producer.QueueMessage(ao.config.Topic, nil, sarama.ByteEncoder(msg.GetPayload()))
 			if err != nil {
 				pack.Recycle()
 				or.LogError(err)
 				break
 			}
-		}
 		case err = <-errChan:
 			break
+		}
 	}
 	return
 }
@@ -73,14 +66,21 @@ func (ao *KafkaOutput) Run(or OutputRunner, h PluginHelper) (err error) {
 func (ao *KafkaOutput) CleanupForRestart() {
 	ao.client.Close()
 	ao.producer.Close()
-	init()
+	ao.init()
 }
 
-func (ao *KafkaOutput) init() {
-	cconf = sarama.NewClientConfig()
-	ao.client = sarama.NewClient(ao.config.Id, ao.addrs, cconf)
-	kconf = sarama.NewProducerConfig()
-	ao.producer = sarama.NewProducer(ao.client, kconf)
+func (ao *KafkaOutput) init() (err error) {
+	cconf := sarama.NewClientConfig()
+	ao.client, err = sarama.NewClient(ao.config.Id, ao.addrs, cconf)
+	if err != nil {
+		return
+	}
+	kconf := sarama.NewProducerConfig()
+	ao.producer, err = sarama.NewProducer(ao.client, kconf)
+	if err != nil {
+		return
+	}
+	return
 }
 
 func init() {
